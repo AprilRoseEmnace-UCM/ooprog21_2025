eb83766d7a389dc3eae5304b2fa47e98cb28f942
April Rose Emnace <aprilemnace2006@gmail.com>
2025-10-07 02:13:59 -0400

    Add files via upload

---
+++ Chapter_5/Employee.java
@@ -0,0 +1,29 @@
+public class Employee {
+    private int employeeNumber;
+    private double payRate;
+    private final int MAX_EMPLOYEE_NUMBER = 9999;
+    private final double MAX_RATE = 60.00;
+    private final double OVERTIME_MULTIPLIER = 1.5;
+
+    public Employee(int employeeNumber, double payRate) {
+        if (employeeNumber > MAX_EMPLOYEE_NUMBER)
+            this.employeeNumber = MAX_EMPLOYEE_NUMBER;
+        else
+            this.employeeNumber = employeeNumber;
+
+        if (payRate > MAX_RATE)
+            this.payRate = MAX_RATE;
+        else
+            this.payRate = payRate;
+    }
+
+    public double calculateRegularPay(double hoursWorked) {
+        double regularHours = Math.min(40, hoursWorked);
+        return regularHours * payRate;
+    }
+
+    public double calculateOvertimePay(double hoursWorked) {
+        double overtimeHours = Math.max(0, hoursWorked - 40);
+        return overtimeHours * (payRate * OVERTIME_MULTIPLIER);
+    }
+}
